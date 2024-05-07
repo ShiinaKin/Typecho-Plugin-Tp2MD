ba7fea98d68f39aac23a233fd288b067f0b10309
mashirot <shiina@sakurasou.io>
2024-05-07 15:40:11 +0800

    style: code format

--- Action.php
+++ Action.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Action.php
  * 
@@ -6,9 +6,10 @@
  * 
  * @author 熊猫小A
  */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+if (!defined('__TYPECHO_ROOT_DIR__'))
+    exit;
 
-class Tp2MD_Action extends Widget_Abstract_Contents implements Widget_Interface_Do 
+class Tp2MD_Action extends Widget_Abstract_Contents implements Widget_Interface_Do
 {
     /**
      * 返回请求的 JSON
@@ -18,14 +19,12 @@ class Tp2MD_Action extends Widget_Abstract_Contents implements Widget_Interface_
     public function action()
     {
         $options = Helper::options()->plugin('Tp2MD');
-        
-        if($_GET['action'] != 'save')
-        {
+
+        if ($_GET['action'] != 'save') {
             echo '指令错误！';
             return;
         }
-        if($_GET['key'] != $options->key)
-        {
+        if ($_GET['key'] != $options->key) {
             echo '无效的 Key！';
             return;
         }
--- Plugin.php
+++ Plugin.php
@@ -10,9 +10,10 @@
  * @link https://www.imalan.cn
  */
 
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+if (!defined('__TYPECHO_ROOT_DIR__'))
+    exit;
 
-class Tp2MD_Plugin implements Typecho_Plugin_Interface 
+class Tp2MD_Plugin implements Typecho_Plugin_Interface
 {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
@@ -24,7 +25,7 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
     public static function activate()
     {
         // 添加路由
-        Helper::addRoute("route_Tp2MD","/Tp2MD","Tp2MD_Action",'action');
+        Helper::addRoute("route_Tp2MD", "/Tp2MD", "Tp2MD_Action", 'action');
     }
 
     /**
@@ -48,7 +49,9 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
      * @param Typecho_Widget_Helper_Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
 
     /**
      * 获取插件配置面板
@@ -59,15 +62,16 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
      */
     public static function config(Typecho_Widget_Helper_Form $form)
     {
-?>
-<p>
-使用方法：设置好插件后访问 <mark><?php Helper::options()->index('/Tp2MD?action=save&key=[KEY]'); ?></mark> 即可。记住将 [KEY] 替换为插件中的设置的值。<br>
-<mark>请保证 插件目录/cache 文件夹可写</mark>
-</p>
-<?php
+        ?>
+        <p>
+            使用方法：设置好插件后访问 <mark><?php Helper::options()->index('/Tp2MD?action=save&key=[KEY]'); ?></mark> 即可。记住将 [KEY]
+            替换为插件中的设置的值。<br>
+            <mark>请保证 插件目录/cache 文件夹可写</mark>
+        </p>
+        <?php
         $t = new Typecho_Widget_Helper_Form_Element_Select(
             'postInFolder',
-            array('true' => '是','false' => '否'),
+            array('true' => '是', 'false' => '否'),
             'true',
             '文章按分类保存至文件夹',
             '是否按分类将文章保存至文件夹。'
@@ -87,13 +91,14 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
     /**
      * 生成随机字符串作为 key
      */
-    private static function generateRandomString($length = 10) { 
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
-        $randomString = ''; 
-        for ($i = 0; $i < $length; $i++) { 
-            $randomString .= $characters[rand(0, strlen($characters) - 1)]; 
-        } 
-        return $randomString; 
+    private static function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
     }
 
     /**
@@ -105,21 +110,20 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
         $db = Typecho_Db::get();
         $options = Helper::options()->plugin('Tp2MD');
         $folder = $options->postInFolder == 'true';
-        
+
         // 创建路径
         $d = date('Y-m-d-H-i-s');
-        $pathBase = __DIR__.'/cache/'.$d;
+        $pathBase = __DIR__ . '/cache/' . $d;
         mkdir($pathBase);
-        $pagePath = $pathBase.'/page/';
+        $pagePath = $pathBase . '/page/';
         mkdir($pagePath);
-        $postPath = $pathBase.'/post/';
+        $postPath = $pathBase . '/post/';
         mkdir($postPath);
-        if($folder)
-        {
+        if ($folder) {
             $rows = $db->fetchAll($db->select()->from('table.metas')
                 ->where('table.metas.type = ?', 'category'));
             foreach ($rows as $row) {
-                mkdir($postPath.$row['name']);
+                mkdir($postPath . $row['name']);
             }
         }
 
@@ -147,51 +151,52 @@ class Tp2MD_Plugin implements Typecho_Plugin_Interface
             $cates = array();
             $tags = array();
             $relations = $db->fetchAll($db->select()->from('table.relationships')
-                ->where('table.relationships.cid = ?',$row['cid']));
+                ->where('table.relationships.cid = ?', $row['cid']));
             foreach ($relations as $relation) {
                 $meta = $db->fetchRow($db->select()->from('table.metas')
-                    ->where('table.metas.mid = ?',$relation['mid']));
-                if($meta['type'] == 'tag') $tags[] = $meta['name'];
-                if($meta['type'] == 'category') $cates[] = $meta['name'];
+                    ->where('table.metas.mid = ?', $relation['mid']));
+                if ($meta['type'] == 'tag')
+                    $tags[] = $meta['name'];
+                if ($meta['type'] == 'category')
+                    $cates[] = $meta['name'];
             }
             $front_matter['categories'] = $cates;
             $front_matter['tags'] = $tags;
 
             // 自定义字段
             $fields = $db->fetchAll($db->select()->from('table.fields')
-                ->where('table.fields.cid = ?',$row['cid']));
+                ->where('table.fields.cid = ?', $row['cid']));
             foreach ($fields as $field) {
-                $front_matter[$field['name']] = $field[$field['type'].'_value'];
+                $front_matter[$field['name']] = $field[$field['type'] . '_value'];
             }
 
-            $output = "---".PHP_EOL;
+            $output = "---" . PHP_EOL;
             foreach ($front_matter as $key => $value) {
-                if($key == 'categories' || $key == 'tags'){
-                    $output .= $key.': '.PHP_EOL;
+                if ($key == 'categories' || $key == 'tags') {
+                    $output .= $key . ': ' . PHP_EOL;
                     foreach ($value as $v) {
-                        $output .= '  - '.$v.PHP_EOL;
+                        $output .= '  - ' . $v . PHP_EOL;
                     }
-                }else{
-                    $output .= $key.': '.$value.PHP_EOL;
+                } else {
+                    $output .= $key . ': ' . $value . PHP_EOL;
                 }
             }
-            $output .= "---".PHP_EOL.PHP_EOL.PHP_EOL;
+            $output .= "---" . PHP_EOL . PHP_EOL . PHP_EOL;
             $output .= str_replace('<!--markdown-->', '', $row['text']);
 
-            $fileName = str_replace(array(' ','/','|'), '_', $row['title']);
-            $fileName = date('Y-m-d-', $row['created']).$fileName.'.md';
+            $fileName = str_replace(array(' ', '/', '|'), '_', $row['title']);
+            $fileName = date('Y-m-d-', $row['created']) . $fileName . '.md';
             $filePath = '';
-            if($row['type'] == 'page'){
+            if ($row['type'] == 'page') {
                 $filePath = $pagePath;
-            }
-            else {
+            } else {
                 $filePath = $postPath;
-                if($folder) $filePath .= $cates[0].'/';
+                if ($folder)
+                    $filePath .= $cates[0] . '/';
             }
             $filePath .= $fileName;
-            
-            if(!file_put_contents($filePath,$output))
-            {
+
+            if (!file_put_contents($filePath, $output)) {
                 echo '不能写入文件，请检查 cache 文件夹权限！';
                 die();
             }
